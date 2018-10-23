cc75d98c151d16e9bf0b363b3db0c6ed0293e18d
Bret Ikehara <bret@publica-project.com>
2018-10-23 10:41:25 -0700

    allow imports to have priority ordering

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 var path = require('path');
 var fs = require('fs');
 
+function transformPaths(paths) {
+	if (Array.isArray(paths)) {
+		return paths;
+	}
+	return Object.keys(paths).map((key) => {
+		return {
+			key,
+			path: paths[key],
+		};
+	});
+}
+
 module.exports = function rollupPluginImportAlias(options) {
 	if (typeof options !== 'object') {
 		return {};
 	}
 
 	var extensions = (options.Extensions || ['js']);
-	var paths = options.Paths || {};
+	var paths = transformPaths(options.Paths || {});
 	return {
 		resolveId: function(importee, importer) {
 			var extCount = extensions.length;
-			for (var key in paths) {
-				if (importee.substring(0, key.length) === key) {
-					var directory = importee.replace(key, paths[key]);
+			for (var entry of paths) {
+				if (importee.substring(0, entry.key.length) === entry.key) {
+					var directory = importee.replace(entry.key, entry.path);
 					var ext, absolute;
 					for (var i = 0; i < extCount; i++) {
 						ext = extensions[i];
